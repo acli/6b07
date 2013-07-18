e7443591c50539c6aa419a5555b26789e8bd9cf5
Ambrose Li <ambrose.li@gmail.com>
2013-07-18 15:52:00 -0400

    Make rate based on LDR reading, imported from servo_movement_study_2

--- servo_movement_study_2/servo_movement_study_2.ino
+++ servo_movement_study_2/servo_movement_study_2.ino
@@ -7,16 +7,20 @@
 // Yellow = White = Signal
 // http://helifreak.com/showthread.php?t=151527
 
+// +5V to 1kΩ, (1kΩ, LDR) to signal, LDR to GND
+// http://hobbybotics.com/tutorials/tutorial-use-a-light-dependent-resistor-ldr-to-measure-light-levels/
+
 #include <Servo.h> 
+
+const int ldr_pin = A0;
+const int servo_pin = 9;
  
 Servo myservo;  // create servo object to control a servo 
  
-int potpin = 0;  // analog pin used to connect the potentiometer
-int val;    // variable to read the value from the analog pin 
  
 void setup() 
 { 
-  myservo.attach(9);  // attaches the servo on pin 9 to the servo object 
+  myservo.attach(servo_pin); 
 } 
 
 
@@ -24,17 +28,19 @@ void loop()
 {
   static int j;
   static int dir = 1;
+  int light_level = analogRead(ldr_pin);
+  int rate = light_level/100;
   myservo.write(j);
   if (dir > 0) {
-    j += dir;
+    j += dir*rate;
     if (j > 180) {
       dir = -1;
     }
   } else {
-    j += dir;
+    j += dir*rate;
     if (j < 0) {
       dir = +1;
     }
   }
-  delay(5);
+  delay(10);
 } 
