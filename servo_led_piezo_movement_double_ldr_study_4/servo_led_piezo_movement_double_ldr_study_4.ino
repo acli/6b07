b2a90dde7e043faea87a3e27a39c996295caa907
Ambrose Li <ambrose.li@gmail.com>
2013-07-25 16:55:00 -0400

    Two more LED's, revised history code, imported from servo_led_piezo_movement_double_ldr_study_4

--- servo_led_piezo_movement_double_ldr_study_4/servo_led_piezo_movement_double_ldr_study_4.ino
+++ servo_led_piezo_movement_double_ldr_study_4/servo_led_piezo_movement_double_ldr_study_4.ino
@@ -16,6 +16,8 @@ const int ldr_2_pin = A1;
 const int led_1_pin = 3;
 const int led_2_pin = 4;
 const int led_3_pin = 5;
+const int led_4_pin = 6;
+const int led_5_pin = 7;
 const int servo_pin = 9;
 const int speaker_pin = 10;               
 
@@ -24,6 +26,9 @@ const int servo_high = 80;
 
 const float one_second = 1000000.0;
 float tempo = 60.0;
+
+const float SERVO_MIN_RATE = 0.4;
+const float SERVO_MAX_RATE = 7.5;
 
 // frequencies from http://www.phy.mtu.edu/~suits/notefreqs.html
 #define b3 246.94
@@ -64,6 +69,8 @@ void setup() {
   pinMode(led_1_pin, OUTPUT);
   pinMode(led_2_pin, OUTPUT);
   pinMode(led_3_pin, OUTPUT);
+  pinMode(led_4_pin, OUTPUT);
+  pinMode(led_5_pin, OUTPUT);
   pinMode(speaker_pin, OUTPUT);
   myservo.attach(servo_pin); 
   Serial.begin(9600);
@@ -83,27 +90,51 @@ int average_light_level_300s;
 void read_sensors() {
   front_light_level = analogRead(ldr_1_pin);
   reference_light_level = analogRead(ldr_2_pin);
-  estimated_light_level = front_light_level - reference_light_level;
+  float ref1 = average_light_level_5s;
+  float ref2 = average_light_level_60s;
+  const float ref1_weight = 0.75;
+  estimated_light_level = (ref1? front_light_level - ref1: 0) * ref1_weight
+                        + (ref2? front_light_level - ref2: 0) * (1.0 - ref1_weight);
   
   static unsigned long second_start;
-  static int fractional_total = 0;
-  static float fractional_readings = 0;
+  static float fractional_total = 0;
+  static int fractional_readings = 0;
   unsigned long t = millis();
   if (!second_start || t >= second_start + 1000) {
     if (actual_history_size >= LIGHT_LEVEL_HISTORY) {
-      for (int i = 1; i < LIGHT_LEVEL_HISTORY; i += 1) {
-        light_level_history[i - 1] = light_level_history[i];
-      } /* for */
-      actual_history_size -= 1;
+      actual_history_size = LIGHT_LEVEL_HISTORY;
     } /* if */
-    int fractional_average = fractional_readings? int(fractional_readings/fractional_total + 0.5): front_light_level;
-    light_level_history[actual_history_size] = fractional_average;
+    for (int i = actual_history_size; i > 0; i -= 1) {
+      light_level_history[i] = light_level_history[i - 1];
+    } /* for */
+    int fractional_average = fractional_readings? int(fractional_total/fractional_readings + 0.5): front_light_level;
+    light_level_history[0] = fractional_average;
+    actual_history_size += 1;
     if (!second_start) {
       second_start = t;
     } else {
       second_start += 1000;
     } /* if */
     fractional_readings = 0;
+    fractional_total = 0;
+    //Serial.print("History:"); for (int i = 0; i < actual_history_size; i += 1) { Serial.print(" "); Serial.print(light_level_history[i]); } Serial.println("");
+    
+    float total_5s, total_60s, total_300s;
+    for (int i = 0; i < actual_history_size; i += 1) {
+      if (i < 5) {
+        total_5s += light_level_history[i];
+      } /* if */
+      if (i < 60) {
+        total_60s += light_level_history[i];
+      } /* if */
+      if (i < 300) {
+        total_300s += light_level_history[i];
+      } /* if */
+    } /* for */
+    average_light_level_5s = total_5s/(actual_history_size > 5? 5: actual_history_size) + 0.5;
+    average_light_level_60s = total_60s/(actual_history_size > 60? 60: actual_history_size) + 0.5;
+    average_light_level_300s = total_300s/(actual_history_size > 300? 300: actual_history_size) + 0.5;
+    Serial.print("Averages: "); Serial.print(average_light_level_5s); Serial.print(" "); Serial.print(average_light_level_60s); Serial.print(" "); Serial.println(average_light_level_300s);
   } else {
     fractional_readings += 1;
     fractional_total += front_light_level;
@@ -130,35 +161,36 @@ void do_servo_action () {
   static int j = 0;
   static int j_last;
   static int dir = 1;
-  int light_level = estimated_light_level;
-  if (light_level < 0) { // in case the box is lying sideways
-    light_level = -light_level;
-  }
-  float rate = pow(light_level, 1.618)/2000.0;
-  Serial.print(front_light_level);
-  Serial.print(" ");
-  Serial.print(reference_light_level);
-  Serial.print(" -> ");
-  Serial.print(light_level);
-  Serial.print(" -> ");
-  Serial.print(rate);
-  Serial.println("");
-  j = int(j0);
-  if (j != j_last) {
-    myservo.write(j);
-    j_last = j;
-  }
-  if (dir > 0) {
-    j0 += dir*rate;
-    if (j0 > servo_high) {
-      j0 = servo_high;
-      dir = -1;
+  if (estimated_light_level) {
+    int light_level = estimated_light_level;
+    if (light_level < 0) { // in case the box is lying sideways
+      light_level = -light_level;
     }
-  } else {
-    j0 += dir*rate;
-    if (j0 < servo_low) {
-      j0 = servo_low;
-      dir = +1;
+    float rate = pow(light_level, 1.618)/100.0;
+    if (reference_light_level < 1000 && rate < SERVO_MIN_RATE) {
+      rate = SERVO_MIN_RATE;
+    } /* if */
+    if (rate > SERVO_MAX_RATE) {
+      rate = SERVO_MAX_RATE;
+    } /* if */
+    Serial.print(front_light_level); Serial.print(" "); Serial.print(reference_light_level); Serial.print(" ("); Serial.print(average_light_level_60s); Serial.print(") -> "); Serial.print(light_level); Serial.print(" -> "); Serial.print(rate); Serial.println("");
+    j = int(j0);
+    if (j != j_last) {
+      myservo.write(j);
+      j_last = j;
+    }
+    if (dir > 0) {
+      j0 += dir*rate;
+      if (j0 > servo_high) {
+        j0 = servo_high;
+        dir = -1;
+      }
+    } else {
+      j0 += dir*rate;
+      if (j0 < servo_low) {
+        j0 = servo_low;
+        dir = +1;
+      }
     }
   }
 }
@@ -170,6 +202,8 @@ void do_led_action () {
     digitalWrite(led_1_pin, random(2));
     digitalWrite(led_2_pin, random(2));
     digitalWrite(led_3_pin, random(2));
+    digitalWrite(led_4_pin, random(2));
+    digitalWrite(led_5_pin, random(2));
     last_blinked = t;
   }
 }
